417fd3fb962ff3f73a8992e972a54162c18bb8e1
Eugene Ilchenko <ilchenko.eugen@gmail.com>
2017-07-04 18:06:06 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {LoginFormComponent} from "./log-in-form/loginForm.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/log-in-form/loginForm.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loginForm-component',
+  templateUrl: './loginForm.component.html',
+  styleUrls: ['./loginForm.component.css']
+})
+export class LoginFormComponent {
+
+  constructor(){
+  }
+
+}
